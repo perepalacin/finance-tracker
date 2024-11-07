c9936ed55fd4182f1f7b4f9aab1b780c23f7c297
Pere Palacín <perepalacin@gmail.com>
2024-11-07 09:51:31 +0100

    Revert "set httpOnly cookies on login"
    This reverts commit 6d7307040d743b6ad9c5bb9c0ffbfac8eace16cb.

--- frontend/src/components/auth/SignUpForm.tsx
+++ frontend/src/components/auth/SignUpForm.tsx
@@ -21,12 +21,14 @@ import {
     FormMessage,
   } from "../ui/form"
 import { toast } from "@/hooks/use-toast";
+import { ToastAction } from "@radix-ui/react-toast";
 
 
 const SignUpForm = () => {
 
     const location = useLocation();
     const currentPath = location.pathname.split("/")[2];
+    console.log(currentPath);
     const navigate = useNavigate();
     
     const [username, setUsername] = useState("");
@@ -83,7 +85,7 @@ const SignUpForm = () => {
         axios.post('/api/v1/auth/sign-in', {
             username: username,
             password: password,
-          }, { withCredentials: true })
+          })
           .then(function (response) {
             if (response.status === 200) {
                 console.log(response);
@@ -91,11 +93,11 @@ const SignUpForm = () => {
             }
           })
           .catch(function (error) {
-            if (error.status === 403) {
+            if (error.status === 409) {
                 toast({
                     variant: 'destructive',
-                    title: "Wrong credentials",
-                    description: "Username or password are incorrect, try again",
+                    title: "Username taken",
+                    description: "This username already exists, please try another one",
                 });
             } else {
                 toast({
