116d4e702a77d24cf43fae51427828c19691ee9a
Pere Palacín <perepalacin@gmail.com>
2026-01-04 12:36:03 +0100

    fix: small number format bug

--- frontend/src/widgets/CategoriesWidget.tsx
+++ frontend/src/widgets/CategoriesWidget.tsx
@@ -1,101 +1,131 @@
-import AddExpenseModal from '@/components/modals/AddExpenseModal';
-import { Card } from '@/components/ui/card';
-import { useUserData } from '@/context/UserDataContext'
-import { AdminApi } from '@/helpers/Api';
-import { ExpensesCategoryWithAmountDto } from '@/types';
-import { useEffect, useState } from 'react';
+import AddExpenseModal from "@/components/modals/AddExpenseModal";
+import { Card } from "@/components/ui/card";
+import { useUserData } from "@/context/UserDataContext";
+import { AdminApi } from "@/helpers/Api";
+import { ExpensesCategoryWithAmountDto } from "@/types";
+import { useEffect, useState } from "react";
 
 interface ExpenseCategoryWidgetDataProps {
-    id: string;
-    iconName: number;
-    categoryName: string;
-    totalAmount: number;
-    monthlyAmount: number;
+  id: string;
+  iconName: number;
+  categoryName: string;
+  totalAmount: number;
+  monthlyAmount: number;
 }
 
 const CategoriesWidget = () => {
+  const [expensesByCategoriesDto, setExpensesByCategoriesDto] = useState<
+    ExpenseCategoryWidgetDataProps[]
+  >([]);
+  const { expenseCategories, bankAccounts } = useUserData();
 
-    const [expensesByCategoriesDto, setExpensesByCategoriesDto] = useState<ExpenseCategoryWidgetDataProps[]>([]);
-    const {expenseCategories, bankAccounts} = useUserData();
+  const totalExpenses = bankAccounts.reduce(
+    (acc, account) => acc + account.totalExpenses,
+    0
+  );
 
-    const totalExpenses = bankAccounts.reduce((acc, account) => acc + account.totalExpenses, 0);
-
-    useEffect(() => {
-
-        const onSuccessFetchExpensesByCategories = (data: ExpensesCategoryWithAmountDto[]) => {
-            const newExpensesByCategoriesDto: ExpenseCategoryWidgetDataProps[] = [];
-            data.forEach((category) => {
-                const expenseCategory = expenseCategories.find((c) => c.id === category.expenseCategoryId);
-                if (expenseCategory) {
-                    newExpensesByCategoriesDto.push({
-                        id: category.expenseCategoryId,
-                        iconName: Number(expenseCategory.iconName),
-                        categoryName: expenseCategory.categoryName,
-                        totalAmount: category.totalAmount,
-                        monthlyAmount: category.monthlyAmount
-                    });
-                }
-            });
-
-            if (newExpensesByCategoriesDto.length > 0) {
-                setExpensesByCategoriesDto(newExpensesByCategoriesDto);
-            }
+  useEffect(() => {
+    const onSuccessFetchExpensesByCategories = (
+      data: ExpensesCategoryWithAmountDto[]
+    ) => {
+      const newExpensesByCategoriesDto: ExpenseCategoryWidgetDataProps[] = [];
+      data.forEach((category) => {
+        const expenseCategory = expenseCategories.find(
+          (c) => c.id === category.expenseCategoryId
+        );
+        if (expenseCategory) {
+          newExpensesByCategoriesDto.push({
+            id: category.expenseCategoryId,
+            iconName: Number(expenseCategory.iconName),
+            categoryName: expenseCategory.categoryName,
+            totalAmount: category.totalAmount,
+            monthlyAmount: category.monthlyAmount,
+          });
         }
+      });
 
-        const api = new AdminApi();
-        api.sendRequest("GET", "/api/v1/dashboard/category-expenses", {onSuccessFunction: onSuccessFetchExpensesByCategories})
-    }, [expenseCategories, bankAccounts]);
+      if (newExpensesByCategoriesDto.length > 0) {
+        setExpensesByCategoriesDto(newExpensesByCategoriesDto);
+      }
+    };
 
-    if (expensesByCategoriesDto.length === 0) {
-        return (
-            <div className='grid grid-cols-1 w-full gap-2'>
-                <Card className='p-4 flex flex-col gap-2 items-start justify-center w-full'>
-                <div className='flex flex-row justify-start gap-2'>
-                    <h1>Nothing to see here...</h1>
-                </div>
-                <div className='flex flex-col gap-2 w-full'>
-                    <div className='flex flex-col w-full justify-start items-start gap-4'>
-                        <p className='text-sm'>Create expenses to be able to see this widget</p>
-                        <div className='w-1/2'>
-                            <AddExpenseModal
-                                variant="default"
-                                isMainButton={false}
-                                isMainLayoutButton={false}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </Card>
-        </div>
-        )
-    } 
-    
+    const api = new AdminApi();
+    api.sendRequest("GET", "/api/v1/dashboard/category-expenses", {
+      onSuccessFunction: onSuccessFetchExpensesByCategories,
+    });
+  }, [expenseCategories, bankAccounts]);
+
+  if (expensesByCategoriesDto.length === 0) {
     return (
-    <div className='grid grid-cols-1 lg:grid-cols-2 w-full gap-2'>
-        {expensesByCategoriesDto.map((category: ExpenseCategoryWidgetDataProps) => 
-        <Card key={category.id} className='p-4 flex flex-col gap-2 items-start justify-center w-full'>
-        <div className='flex flex-row justify-start gap-2'>
-            <p>{String.fromCodePoint(category.iconName)}</p>
-            <h1>{category.categoryName}</h1>
-        </div>
-        <div className='flex flex-col gap-2 w-full'>
-            <div className='flex flex-row w-full justify-between items-center gap-8'>
-                <p className='text-muted-foreground text-sm'>Total:</p>
-                <p className='text-sm'>{new Intl.NumberFormat("en-US", { style: "currency", currency: "EUR"}).format(category.totalAmount)}</p>
+      <div className="grid grid-cols-1 w-full gap-2">
+        <Card className="p-4 flex flex-col gap-2 items-start justify-center w-full">
+          <div className="flex flex-row justify-start gap-2">
+            <h1>Nothing to see here...</h1>
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex flex-col w-full justify-start items-start gap-4">
+              <p className="text-sm">
+                Create expenses to be able to see this widget
+              </p>
+              <div className="w-1/2">
+                <AddExpenseModal
+                  variant="default"
+                  isMainButton={false}
+                  isMainLayoutButton={false}
+                />
+              </div>
             </div>
-            <div className='flex flex-row w-full justify-between items-center gap-8'>
-                <p className='text-muted-foreground text-sm'>Monthly:</p>
-                <p className='text-sm'>{new Intl.NumberFormat("en-US", { style: "currency", currency: "EUR"}).format(category.monthlyAmount)}</p>
-            </div>
-            <div className='flex flex-row w-full justify-between items-center gap-8'>
-                <p className='text-muted-foreground text-sm'>Percent (total):</p>
-                <p className='text-sm'>{(category.totalAmount/totalExpenses).toFixed(2)}%</p>
-            </div>
-        </div>
-    </Card>
-)}    
-    </div>
-  )
-}
+          </div>
+        </Card>
+      </div>
+    );
+  }
 
-export default CategoriesWidget
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 w-full gap-2">
+      {expensesByCategoriesDto.map(
+        (category: ExpenseCategoryWidgetDataProps) => (
+          <Card
+            key={category.id}
+            className="p-4 flex flex-col gap-2 items-start justify-center w-full"
+          >
+            <div className="flex flex-row justify-start gap-2">
+              <p>{String.fromCodePoint(category.iconName)}</p>
+              <h1>{category.categoryName}</h1>
+            </div>
+            <div className="flex flex-col gap-2 w-full">
+              <div className="flex flex-row w-full justify-between items-center gap-8">
+                <p className="text-muted-foreground text-sm">Total:</p>
+                <p className="text-sm">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(category.totalAmount)}
+                </p>
+              </div>
+              <div className="flex flex-row w-full justify-between items-center gap-8">
+                <p className="text-muted-foreground text-sm">Monthly:</p>
+                <p className="text-sm">
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(category.monthlyAmount)}
+                </p>
+              </div>
+              <div className="flex flex-row w-full justify-between items-center gap-8">
+                <p className="text-muted-foreground text-sm">
+                  Percent (total):
+                </p>
+                <p className="text-sm">
+                  {((category.totalAmount / totalExpenses) * 100).toFixed(2)}%
+                </p>
+              </div>
+            </div>
+          </Card>
+        )
+      )}
+    </div>
+  );
+};
+
+export default CategoriesWidget;
